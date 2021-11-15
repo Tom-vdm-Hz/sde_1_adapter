a7cdd1840b0f83997c6f8aa023f77444fa2b901d
Tom-vdm-Hz <tom.v.d.muijsenberg@gmail.com>
2021-11-15 12:53:02 +0100

    removed comment

--- src/com/hz/Main.java
+++ src/com/hz/Main.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
         ConsoleReader reader = new ConsoleReader();
         ConsoleWriter writer = new ConsoleWriter();
